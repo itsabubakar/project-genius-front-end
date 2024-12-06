32daf14151623964a7d79e09aa854807e8f66b86
Sadiq Bilyamin <seeker99157@gmail.com>
2024-12-06 22:26:01 +0100

    chore: setup frontend repo

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main>
+        <h1 className="text-3xl font-bold text-purple-500">
+          Welcome to Project Genius Front end
+        </h1>
+      </main>
+    </div>
+  );
+}
