305e5c73b653a6af11d0b8993cb2810f750b1b18
hamidusman <abdulhamidusman218@gmail.com>
2025-01-02 16:06:32 +0100

    progress data for iteration

---
+++ app/data/progressData.js
@@ -0,0 +1,28 @@
+const progressData = [
+    {
+      id: 1,
+      title: "Registration Phase",
+      description: "In this phase, contestants register their teams.",
+      isActive: true,
+    },
+    {
+      id: 2,
+      title: "Prototype Development Phase",
+      description: "Step description",
+      isActive: false,
+    },
+    {
+      id: 3,
+      title: "Grand Finale",
+      description: "Step description",
+      isActive: false,
+    },
+    {
+      id: 4,
+      title: "Post Event Phase",
+      description: "Step description",
+      isActive: false,
+    },
+  ];
+
+export default progressData
