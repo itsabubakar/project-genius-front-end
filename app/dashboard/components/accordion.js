8e7b8669ae49e87805477bccce7b068e72968662
hamidusman <abdulhamidusman218@gmail.com>
2025-01-09 17:39:09 +0100

    size adjustment for header and subtitle

---
+++ app/dashboard/components/accordion.js
@@ -0,0 +1,13 @@
+export default function Accordion() {
+    return (
+        <article className="flex">
+            <div>
+                +
+            </div>
+
+            <h4>hello world</h4>
+
+
+        </article>
+    )
+}
--- app/dashboard/sections/help.js
+++ app/dashboard/sections/help.js
@@ -2,14 +2,14 @@ export default function Help() {
     return(
         <section className="flex flex-col px-3 py-6 gap-4">
                 
-            <div className="flex flex-col gap-6 items-center">
-                <h2 className="text-[32px] font-bold">Find The Help You Need</h2>
+            <div className="flex flex-col gap-6 items-center md:items-start">
+                <h2 className="text-[32px] md:text-[44px] font-bold">Find The Help You Need</h2>
                 <br />
-                <h5 className="text-[24px] font-medium spaci">Frequently Asked Questions</h5>
+                <h5 className="text-[24px] md:text-[28px] font-medium spaci">Frequently Asked Questions</h5>
 
             </div>
 
-            kk
+            
         </section>
     )
 }
