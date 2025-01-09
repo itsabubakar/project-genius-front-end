879549e42ecd046c3e01c7a0e936d84f3c6118bb
hamidusman <abdulhamidusman218@gmail.com>
2025-01-09 20:38:07 +0100

    accordion design

--- app/dashboard/components/accordion.js
+++ app/dashboard/components/accordion.js
@@ -1,13 +1,31 @@
-export default function Accordion() {
+import { useState } from "react"
+
+export default function Accordion({question, response}) {
+    const [accordion, openAccordion] = useState(false)
+
+    const handleAccordion = () => {
+        if(!accordion) {
+            openAccordion(true)
+        }
+        else {
+            openAccordion(false)
+        }
+    }
     return (
-        <article className="flex">
+        <article
+            onClick={handleAccordion}
+            className={`text-primary_pressed flex gap-3  p-4 shadow-lg rounded-lg`}>
             <div>
                 +
             </div>
+            <div  className=" flex flex-col flex-wrap">
+                
 
-            <h4>hello world</h4>
+            <h4 className="flex flex-col gap-2">{question}</h4>
+            <p className={` ${accordion ? "block" : "hidden"} text-black mt-2`}>{response}</p>
 
 
+            </div>
         </article>
     )
 }
--- app/dashboard/sections/help.js
+++ app/dashboard/sections/help.js
@@ -1,6 +1,7 @@
+import Accordion from "../components/accordion"
 export default function Help() {
     return(
-        <section className="flex flex-col px-3 py-6 gap-4">
+        <section className="flex flex-col px-3 py-6 gap-8">
                 
             <div className="flex flex-col gap-6 items-center md:items-start">
                 <h2 className="text-[32px] md:text-[44px] font-bold">Find The Help You Need</h2>
@@ -8,6 +9,8 @@ export default function Help() {
                 <h5 className="text-[24px] md:text-[28px] font-medium spaci">Frequently Asked Questions</h5>
 
             </div>
+
+            <Accordion/>
 
             
         </section>
