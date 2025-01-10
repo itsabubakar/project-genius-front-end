0b17f8ae6ad4efa6ebe8af6e4d89d0a0a6b88b2c
hamidusman <abdulhamidusman218@gmail.com>
2025-01-10 11:32:55 +0100

    added conditional icons to the accordion

--- app/dashboard/components/accordion.js
+++ app/dashboard/components/accordion.js
@@ -1,14 +1,16 @@
 import { useState } from "react"
-
+import Image from "next/image"
+import minus from "../../../public/icons/minus.svg"
+import plus from "../../../public/icons/plus.svg"
 export default function Accordion({question, response}) {
-    const [accordion, openAccordion] = useState(false)
+    const [openAccordion, setOpenAccordion] = useState(false)
 
     const handleAccordion = () => {
-        if(!accordion) {
-            openAccordion(true)
+        if(!openAccordion) {
+            setOpenAccordion(true)
         }
         else {
-            openAccordion(false)
+            setOpenAccordion(false)
         }
     }
     return (
@@ -16,13 +18,13 @@ export default function Accordion({question, response}) {
             onClick={handleAccordion}
             className={`text-primary_pressed flex gap-3  p-4 shadow-lg rounded-lg`}>
             <div>
-                +
+                {!openAccordion ? <Image src={plus} />: <Image src={minus} />}
             </div>
             <div  className=" flex flex-col flex-wrap">
                 
 
             <h4 className="flex flex-col gap-2">{question}</h4>
-            <p className={` ${accordion ? "block" : "hidden"} text-black mt-2`}>{response}</p>
+            <p className={` ${openAccordion ? "block" : "hidden"} text-black mt-2`}>{response}</p>
 
 
             </div>
