b7c4d930f2964f46da0ca869a98224d03c8b21a8
sadiq <seeker99157@gmail.com>
2025-01-15 20:17:04 +0100

    Refactor code for consistency; add usePathname hook in RootLayout

--- app/dashboard/sections/help.js
+++ app/dashboard/sections/help.js
@@ -1,45 +1,49 @@
-import Accordion from "../components/accordion"
-import accordionData from "@/app/data/accordionData"
+import Accordion from "../components/accordion";
+import accordionData from "@/app/data/accordionData";
 
-import Image from "next/image"
+import Image from "next/image";
 
-import mail from "../../../public/icons/black_email.svg"
-import phone from "../../../public/icons/black_phone.svg"
+import mail from "../../../public/icons/black_email.svg";
+import phone from "../../../public/icons/black_phone.svg";
 export default function Help() {
-    return(
-        <section className=" px-3 py-6 flex flex-col gap-16">
-        
-        <div className="flex flex-col gap-8">
-                
-            <div className="flex flex-col gap-6 items-center md:items-start">
-                <h2 className="text-[32px] md:text-[44px] font-bold">Find The Help You Need</h2>
-                <br />
-                <h5 className="text-[24px] md:text-[28px] font-medium spaci">Frequently Asked Questions</h5>
-
-            </div>
-
-            {accordionData.map((data, index) =>(
-                <Accordion
-                    key={index}
-                    question={data.question}
-                    answer={data.answer}
-                />
-            ))}
-
-            
+  return (
+    <section className=" px-3 py-6 flex flex-col gap-16">
+      <div className="flex flex-col gap-8">
+        <div className="flex flex-col gap-6 items-center md:items-start">
+          <h2 className="text-[32px] md:text-[44px] font-bold">
+            Find The Help You Need
+          </h2>
+          <br />
+          <h5 className="text-[24px] md:text-[28px] font-medium">
+            Frequently Asked Questions
+          </h5>
         </div>
 
-        <section className="w-full shadow-md flex flex-col p-4 gap-8 rounded-2xl">
-            <h1 className="font-medium text-[24px]">Still Have Questions? We're Here To Help!</h1>
+        {accordionData.map((data, index) => (
+          <Accordion
+            key={index}
+            question={data.question}
+            answer={data.answer}
+          />
+        ))}
+      </div>
 
-            <ul className="flex flex-col gap-1 p-4 bg-primary_subtle rounded-xl">
-            <li className="inter ">Ufuoma Morgan (Chairman)</li>
-            <li className="inter flex gap-2"><Image src={phone} />+ 234 905 5433 811</li>
-            <li className="inter flex gap-2"><Image src={mail} />example@gmail.com</li>
-            
+      <section className="w-full shadow-md flex flex-col p-4 gap-8 rounded-2xl">
+        <h1 className="font-medium text-[24px]">
+          Still Have Questions? We&rsquo;re Here To Help!
+        </h1>
 
-            </ul>
-
-        </section></section>
-    )
+        <ul className="flex flex-col gap-1 p-4 bg-primary_subtle rounded-xl">
+          <li className="inter ">Ufuoma Morgan (Chairman)</li>
+          <li className="inter flex gap-2">
+            <Image src={phone} />+ 234 905 5433 811
+          </li>
+          <li className="inter flex gap-2">
+            <Image src={mail} />
+            example@gmail.com
+          </li>
+        </ul>
+      </section>
+    </section>
+  );
 }
